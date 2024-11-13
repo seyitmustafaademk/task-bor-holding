014bd82e3b3ea7f16c45575a3511884898234206
Seyit Mustafa Adem Kandemir <seyitmustafaademk@gmail.com>
2024-11-13 22:56:56 +0300

    users table structure has been updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('firstname', 30);
+            $table->string('lastname', 30);
+            $table->string('email', 75)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -22,7 +23,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email', 75)->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
